5a85dc1e6f9da80bada416d5be871276077b0a36
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-02-13 19:48:31 +0100

    Transformed in card

---
+++ src/assets/components/card/ActorCard.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+export default function ActorCard() {
+  const [actors, setActors] = useState([]);
+  const axiosGetActors = () => {
+    axios.get("https://lanciweb.github.io/demo/api/actors/").then((res) => {
+      setActors(res.data);
+      console.log(res.data);
+    });
+  };
+  useEffect(axiosGetActors, []);
+  return (
+    <>
+      {actors.map((element, index) => (
+        <div key={index} className="col-6">
+          <div className="card h-100">
+            <img
+              src={element.image}
+              className="card-img-top "
+              alt={element.name}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{element.name}</h5>
+              <p className="card-text">{element.birth_year}</p>
+              <p className="card-text">{element.nationality}</p>
+              <p className="card-text">{element.biography}</p>
+              <p className="card-text">{element.awards.join(", ")}</p>
+              <h4>Film Famosi</h4>
+              <p className="card-text">{element.known_for.join("- ")}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
